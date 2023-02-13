f26606248edd55f77d695aaa993ac7d924b75a2f
orastvorov <orastvorov@waverleysoftware.com>
2023-02-13 17:20:07 +0200

    setup default server

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT || 5001
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`)
+})
